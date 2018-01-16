2f36c29efd2c978c859db11a9baa54a364ee3bbc
ngsm <ngsm@5CG6352D49.STAFF.HELP.LOCAL>
2018-01-16 09:41:28 +0800

    Lecture 1

---
+++ src/Lecture1/HellowWorld.java
@@ -0,0 +1,13 @@
+package Lecture1;
+
+/**
+ * FIrst demonstration program
+ * @author ngsm
+ */
+public class HellowWorld {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello HELP!");
+    }
+}
