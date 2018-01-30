974e21639a536386f2fb73413947333bfafb1cb7
ngsm <ngsm@5CG6352D49.STAFF.HELP.LOCAL>
2018-01-30 10:29:04 +0800

    Selection

--- src/Lecture3/Voting.java
+++ src/Lecture3/Voting.java
@@ -1,26 +1,29 @@
 package Lecture3;
+
 import java.time.LocalDate;
 import java.util.*;
 
 /**
  * A program to check if the user can vote
+ *
  * @author ngsm
  */
 public class Voting {
-    
-    public static void main(String[]args)
-    {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // ask the user for their year of birth
         System.out.print("Enter your year of birth :");
         int yearOfBirth = sc.nextInt();
         int currentYear = LocalDate.now().getYear();
-        
+
         // calculate age
         int age = currentYear - yearOfBirth;
-        if (age < 21)
+        if (age < 21) {
             System.out.println("Too young to vote");
-        else
+            System.out.println("You're so lucky");
+        } else {
             System.out.println("Register as voter");
+        }
     }
 }
